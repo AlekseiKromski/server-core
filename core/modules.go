b489340085ace07cde95ea240b5af9d2fa9ae1ba
alekseiKromski <aleksei.kromski@outlook.com>
2024-12-10 16:56:19 +0200

    core/logger: added default logger

--- core/modules.go
+++ core/modules.go
@@ -4,6 +4,6 @@ type Module interface {
 	Start(notifyChannel chan struct{}, busEventChannel chan BusEvent, requirements map[string]Module) // Start module
 	Stop()                                                                                            // Stop module
 	Require() []string                                                                                // Require list of required modules
-	Signature() string                                                                                // Signature unique name
-	Log(messages ...string)                                                                           // Log should have log mechanism
+	Signature() string                                                                                // Signature unique name // Log should have log mechanism
+	Logger                                                                                            // Logger custom or core.DefaultLogger logger
 }
