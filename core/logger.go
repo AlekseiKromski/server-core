b298f76c393aa64f590b5a595ab17e19183c039e
alekseiKromski <aleksei.kromski@outlook.com>
2025-04-12 13:04:01 +0300

    core/logger: changed logger functions
    Currently, we should have 3 levels of log
     - ERROR
     - WARN
     - INFO

--- core/logger.go
+++ core/logger.go
@@ -6,7 +6,9 @@ import (
 )
 
 type Logger interface {
-	Log(messages ...string)
+	Info(messages ...string)
+	Error(messages ...string)
+	Warn(messages ...string)
 }
 
 type DefaultLogger struct {
@@ -19,8 +21,20 @@ func NewDefaultLogger(signature string) *DefaultLogger {
 	}
 }
 
-func (dl *DefaultLogger) Log(messages ...string) {
-	logString := fmt.Sprintf("%s: ", dl.signature)
+func (dl *DefaultLogger) Error(messages ...string) {
+	dl.log("ERROR", messages...)
+}
+
+func (dl *DefaultLogger) Warn(messages ...string) {
+	dl.log("WARN", messages...)
+}
+
+func (dl *DefaultLogger) Info(messages ...string) {
+	dl.log("INFO", messages...)
+}
+
+func (dl *DefaultLogger) log(prefix string, messages ...string) {
+	logString := fmt.Sprintf("%s [%s]: ", dl.signature, prefix)
 
 	for index, message := range messages {
 		logString += fmt.Sprintf("%s", message)
@@ -29,5 +43,6 @@ func (dl *DefaultLogger) Log(messages ...string) {
 			logString += ": "
 		}
 	}
+
 	log.Printf(logString)
 }
